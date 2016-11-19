3a95676ec97944fa68aa7fc10d81e0bb7af509c3
Vidit Kothari <kothari.vidit@gmail.com>
2016-11-19 19:29:25 +0530

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -6,13 +6,13 @@ public class MainClass {
         ReportCard rc1=new ReportCard(s1);
 
         // Setting 1st Semester Score
-		rc1.setScore(1,1,80);
-		rc1.setScore(1,2,72);
-		rc1.setScore(1,3,63);
-		rc1.setScore(1,4,54);
+	rc1.setScore(1,1,80);
+	rc1.setScore(1,2,72);
+	rc1.setScore(1,3,63);
+	rc1.setScore(1,4,54);
         rc1.setScore(1,5,45);
 
-		// Setting 2nd Semester Score
+	// Setting 2nd Semester Score
         rc1.setScore(2,1,100);
         rc1.setScore(2,2,90);
         rc1.setScore(2,3,85);
@@ -20,7 +20,8 @@ public class MainClass {
         rc1.setScore(2,5,66);
 
         System.out.print("\n");
-        // Printing the Report card for Student 's1' by simply printing the ReportCard object with Overridden .toString() method
+        
+	// Printing the Report card for Student 's1' by simply printing the ReportCard object with Overridden .toString() method
         System.out.print(rc1.toString());
     }
 }
